186726cc640678bf6b5d66a5ff858a09869cc253
amcrabtree <angela.crabtree.88@gmail.com>
2022-05-06 11:51:06 -0700

    updated many things

--- README.md
+++ README.md
@@ -1,10 +1,87 @@
-# Demultiplexing
+# Demultiplex
+> Demultiplexes reads according to barcodes. Use for single-indexed libraries only.
 
-## [Assignment the First](Assignment-the-first)
-Due Friday, July 30, 2021, 5:00 PM PDT.
+## Table of Contents
+* [General Info](#general-information)
+* [Screenshots](#screenshots)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Acknowledgements](#acknowledgements)
 
-## [Assignment the Second](Assignment-the-second)
-Due on Monday, August 2, 2021, 10:00 AM PDT.
 
-## [Assignment the Third](Assignment-the-third)
-Due on Friday, August 13, 2021, 11:59 PM PDT.
+
+## General Information
+- The main purpose of this project is to separate (demultiplex) each sample from a single NGS sequencing run. 
+- The amount of index swapping and undetermined index-pairs are also determined, before and after quality filtering. 
+- The python script will generate 2 "clean" forward and reverse read files for each sample, as well as two FASTQ files with index-hopped reads-pairs, and two FASTQ files undetermined (non-matching or low quality) index-pairs for the entire run. 
+- The R markdown script will generate 1 pdf file which is a summary report of the filtering process. 
+
+
+## Features
+- A statistical summary report generated in R markdown
+
+
+## Screenshots
+
+#### Distribution Plot
+
+![](./img/histogram.png)
+
+
+
+## Setup
+
+**Dependencies**
+
+python3.9
+> Python packages: argparse, gzip
+matplotlib
+R
+> R packages: knitr
+
+
+
+## Usage
+
+**Running the script**
+
+	$ ./demux.py -h
+	usage: demux.py [-h] -1 R1_FILE -2 R2_FILE -3 R3_FILE -4 R4_FILE -b
+	                BARCODE_FILE [-q MIN_QSCORE] -o OUTPUT_DIR
+
+	Demultiplexes reads according to barcodes. Use for single-indexed libraries
+	only.
+
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -1 R1_FILE, --R1_file R1_FILE
+	                        R1 (forward read) file = normally 'R1'
+	  -2 R2_FILE, --R2_file R2_FILE
+	                        R2 (forward index) file = normally 'I1'
+	  -3 R3_FILE, --R3_file R3_FILE
+	                        R3 (reverse index) file = normally 'I2'
+	  -4 R4_FILE, --R4_file R4_FILE
+	                        R4 (reverse read) file = normally 'R2'
+	  -b BARCODE_FILE, --barcode_file BARCODE_FILE
+	                        file of barcodes
+	  -q MIN_QSCORE, --min_qscore MIN_QSCORE
+	                        minimum qscore allowed; default=20
+	  -o OUTPUT_DIR, --output_dir OUTPUT_DIR
+	                        output folder for fastq files
+
+**Generating the statistical report:**
+
+`Rscript -e 'rmarkdown::render("demux_report.Rmd", params=list(data="REAL-output/bin_counts.csv"))'`
+
+
+## Project Status
+Project is: _complete_ 
+
+
+## Acknowledgements
+- This project was based on instructions given by the University of Oregon's Knight Campus Internship Program for Bioinformatics and Genomics (BGMP)
+- Many thanks to the BGMP instructors and fellow classmates for their feedback
+
+
+
--- demux.py
+++ demux.py
@@ -2,18 +2,17 @@
 
 import argparse
 import gzip
-import Bioinfo
 
 ########################## ARGPARSE ##############################
 # define command line arguments
 def get_args():
-	parser = argparse.ArgumentParser(description="Normalizes the k-mer coverage of your input reads and saves those reads to a new FASTQ file")
+	parser = argparse.ArgumentParser(description="Demultiplexes reads according to barcodes. Use for single-indexed libraries only.")
 	parser.add_argument("-1", "--R1_file", help="R1 (forward read) file", required=True)
-	parser.add_argument("-2", "--R2_file", help="R2 (forward index) file", required=True)
-	parser.add_argument("-3", "--R3_file", help="R3 (reverse index) file", required=True) 
-	parser.add_argument("-4", "--R4_file", help="R4 (reverse read) file", required=True)
+	parser.add_argument("-2", "--R2_file", help="I1 (forward index) file", required=True)
+	parser.add_argument("-3", "--R3_file", help="I2 (reverse index) file", required=True) 
+	parser.add_argument("-4", "--R4_file", help="R2 (reverse read) file", required=True)
 	parser.add_argument("-b", "--barcode_file", help="file of barcodes", required=True)
-	parser.add_argument("-q", "--min_qscore", help="minimum qscore allowed", required=False, type=int)
+	parser.add_argument("-q", "--min_qscore", help="minimum qscore allowed; default=20", required=False, type=int)
 	parser.add_argument("-o", "--output_dir", help="output folder for fastq files", required=True)
 	return parser.parse_args()
 
@@ -30,22 +29,16 @@ dir_out = args.output_dir
 ########################## FUNCTIONS ##############################
 
 def revc (seq: str) -> str:
-    '''converts DNA seq to reverse complement'''
-    seq=seq.upper()
-    nucd = {"A":  "T", "T": "A", "C": "G", "G": "C", "N": "N"}
-    rseq=seq.translate(str.maketrans(nucd))[::-1] 
-    return rseq
+	'''converts DNA seq to reverse complement'''
+	seq=seq.upper()
+	nucd = {"A":  "T", "T": "A", "C": "G", "G": "C", "N": "N"}
+	rseq=seq.translate(str.maketrans(nucd))[::-1] 
+	return rseq
 
-# def bc_correct(qbar_seq: str, barcode_txt: tuple):
-#     '''input is barcode sequence in question and a tuple of possible
-#     barcode values, output is corrected barcode or empty string 
-#     if no correction possible'''
-#     - determine min hamming dist in barcode tuple and save as variable
-#         - initialize hamming dist variable as barcode seq length
-#         - determine hamming dist between each barcode pair
-#         - if dist < variable, replace
-#     - open dust bin files (forward and reverse reads)
-
+def convert_phred(letter: str) -> int:
+    """Converts a single character into a phred score"""
+    return ord(letter)-33
+    
 ########################## MAIN ##############################
 
 ## open barcode file, store barcodes as dictionary, close file. 
@@ -60,25 +53,25 @@ while line:
     line = b.readline().rstrip()
 b.close()
 
-'''
-## open unzipped input files
-fr = open(f_reads, "r")
-rr = open(r_reads, "r")
-fi = open(f_index, "r")
-ri = open(r_index, "r")
-## count lines in file (will be same for all files)
-numrecs = int((max(i for i, _ in enumerate(ri))+1)/4)
-ri = open(r_index, "r")
-'''
-
-## open zipped input files
-fr = gzip.open(f_reads, "rt")
-rr = gzip.open(r_reads, "rt")
-fi = gzip.open(f_index, "rt")
-ri = gzip.open(r_index, "rt")
-## count lines in file (will be same for all files)
-numrecs = int((max(i for i, _ in enumerate(ri))+1)/4)
-ri = gzip.open(r_index, "rt")
+## open input files
+if f_reads.split(".")[-1] == "gz":
+    ## open zipped input files
+    fr = gzip.open(f_reads, "rt")
+    rr = gzip.open(r_reads, "rt")
+    fi = gzip.open(f_index, "rt")
+    ri = gzip.open(r_index, "rt")
+    ## count lines in file (will be same for all files)
+    numrecs = int((max(i for i, _ in enumerate(ri))+1)/4)
+    ri = gzip.open(r_index, "rt")
+else:
+    ## open unzipped input files
+    fr = open(f_reads, "r")
+    rr = open(r_reads, "r")
+    fi = open(f_index, "r")
+    ri = open(r_index, "r")
+    ## count lines in file (will be same for all files)
+    numrecs = int((max(i for i, _ in enumerate(ri))+1)/4)
+    ri = open(r_index, "r")
 
 ## generate/open output files (sample bins [# of barcodes *2], 
 ## swapping bins [2], dust bins [2])
@@ -88,7 +81,7 @@ junk_fw = open (dir_out+"/FW_junk.fq", "a")
 junk_rv = open (dir_out+"/RV_junk.fq", "a")
 
 ## make dictionary of output file info while opening them
-## file_dict - key=FW/RV & index, value=file handle
+## file_dict: key=FW/RV & index, value=file handle
 file_dict = {}
 for el in bar_dict:
     f = dir_out + "/FW_" + el + ".fq"
@@ -97,6 +90,14 @@ for el in bar_dict:
     rvh = "RV_"+el
     file_dict[fwh] = open(f, "w")
     file_dict[rvh] = open(r, "w")
+
+## make dictionary of record counts to sum while binning
+## count_dict: key=bin pair, value=number of record pairs
+count_dict = {}
+count_dict["junk"]=0
+count_dict["swap"]=0
+for el in bar_dict:
+    count_dict[el]=0
 
 ## sort reads by indexes and write to output files
 for i in range(numrecs):
@@ -114,24 +115,27 @@ for i in range(numrecs):
     if bar1 not in bar_dict.values() or bar2 not in bar_dict.values():
         junk_fw.write(new_head_r1+"\n"+r1_lst[1]+"\n"+r1_lst[2]+"\n"+r1_lst[3]+"\n")
         junk_rv.write(new_head_r2+"\n"+r2_lst[1]+"\n"+r2_lst[2]+"\n"+r2_lst[3]+"\n")
+        count_dict["junk"] += 1
     else:
         ## check for Q-scores
         q=0 # flag for bad qscore
         for basepos in range(len(i1_lst[3])):  # check qscore of forward read index
-            p=int(Bioinfo.convert_phred(i1_lst[3][basepos]))
+            p=int(convert_phred(i1_lst[3][basepos]))
             if p < minq:
                 q = 1
         for basepos in range(len(i2_lst[3])):  # check qscore of reverse read index
-            p=int(Bioinfo.convert_phred(i2_lst[3][basepos]))
+            p=int(convert_phred(i2_lst[3][basepos]))
             if p < minq:
                 q = 1
         if q == 1:  # if bad qscore, write to junk bins
             junk_fw.write(new_head_r1+"\n"+r1_lst[1]+"\n"+r1_lst[2]+"\n"+r1_lst[3]+"\n")
             junk_rv.write(new_head_r2+"\n"+r2_lst[1]+"\n"+r2_lst[2]+"\n"+r2_lst[3]+"\n")
+            count_dict["junk"] += 1
         ## check for index swapping
         elif bar1 != bar2:
             swap_fw.write(new_head_r1+"\n"+r1_lst[1]+"\n"+r1_lst[2]+"\n"+r1_lst[3]+"\n")
-            swap_rv.write(new_head_r2+"\n"+r2_lst[1]+"\n"+r2_lst[2]+"\n"+r2_lst[3]+"\n")            
+            swap_rv.write(new_head_r2+"\n"+r2_lst[1]+"\n"+r2_lst[2]+"\n"+r2_lst[3]+"\n") 
+            count_dict["swap"] += 1           
         ## write everything else to sample bins
         else: 
             index_id = [key for key, val in bar_dict.items() if val == bar1][0]
@@ -139,6 +143,7 @@ for i in range(numrecs):
             r = [val for key, val in file_dict.items() if key == "RV_" + index_id][0]
             f.write(new_head_r1+"\n"+r1_lst[1]+"\n"+r1_lst[2]+"\n"+r1_lst[3]+"\n")
             r.write(new_head_r2+"\n"+r2_lst[1]+"\n"+r2_lst[2]+"\n"+r2_lst[3]+"\n")
+            count_dict[index_id] += 1
             
 ## close output files
 swap_fw.close()
@@ -153,3 +158,30 @@ fr.close()
 rr.close() 
 fi.close()
 ri.close()
+
+## print count stats
+for el in count_dict:
+    print("Number of record pairs in", el, "bin:", count_dict[el])
+
+########################## GRAPH ##############################
+
+## print plot
+import matplotlib.pyplot as plt
+xval=[x for x in count_dict.keys()]
+yval=[y for y in count_dict.values()]
+plt.bar(xval, yval, color='blue')
+plt.title("Number of Read Pairs Per Index")
+plt.xlabel("Index/Bin")
+plt.ylabel("Number of Read Pairs")
+plt.savefig(dir_out+"/demux_hist.png", format="png")
+
+########################## OUTPUT DATA ##############################
+
+## write bin values to output file 
+import sys
+cmdline = (' '.join(sys.argv))
+with open(dir_out+"/bin_counts.csv", "a") as mdrpt:
+    mdrpt.write(cmdline + "\n")
+    mdrpt.write("bin,rpcount\n")
+    for el in count_dict:
+        mdrpt.write(el + "," + str(count_dict[el]) + "\n")
